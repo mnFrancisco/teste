8d248ebb89c2032ce315b85ff3ae7eec61479ecb
darckrebon <92961019+darckrebon@users.noreply.github.com>
2022-12-24 13:19:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium fuga tenetur quo cupiditate corporis officiis facere nesciunt provident modi dolor quaerat sint veritatis, magni vel id eveniet! Voluptate, iusto. Nemo!</h1>
+</body>
+</html>
